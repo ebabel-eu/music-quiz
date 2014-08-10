e63b8b25f6b500b10dc0a7781cb126db88b86b48
ebabel-eu <hello@ebabel.eu>
2014-08-10 19:04:12 +0200

    UPDATED signin and signup views.

--- src/js/config.js
+++ src/js/config.js
@@ -3,7 +3,9 @@ musicQuizApp
         // Set a mapping between url routes and views.
         .config(['$routeProvider', function ($routeProvider) {
             $routeProvider
-                .when('/', {controller: 'mainController', templateUrl: 'views/home.html'})
+                .when('/', {controller: 'mainController', templateUrl: 'views/signin.html'})
+                .when('/signin', {controller: 'mainController', templateUrl: 'views/signin.html'})
+                .when('/signup', {controller: 'mainController', templateUrl: 'views/signup.html'})
                 .when('/decibel-account', {templateUrl: 'views/decibelAccount.html'})
                 .when('/signin', {templateUrl: 'views/signinOrSignup.html'})
                 .when('/404', {templateUrl: 'views/404.html'})
