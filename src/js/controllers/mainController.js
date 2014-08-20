ad29440e267c14ef1a1278afedbb278c21be3108
ebabel-eu <hello@ebabel.eu>
2014-08-20 23:57:34 +0200

    ADDED more style for UPDATED homepage.

--- src/js/controllers/mainController.js
+++ src/js/controllers/mainController.js
@@ -10,7 +10,18 @@ musicQuizApp.controller('mainController', ['$scope', '$location',
             name: 'Nadjib'
         };
 
-        $scope.quizCoinBalance = 0;
+        $scope.quizCoins = {
+            // How many Quiz Coins the current gamer holds.
+            balance: 5,
+
+            // At what level does the system think the current game has too few Quiz Coins.
+            danger: 3
+        };
+
+        $scope.notification = {
+            level: 'warning',
+            message: 'Your Quiz Coins balance is a bit low.'
+        };
     }
 
 ]); 
