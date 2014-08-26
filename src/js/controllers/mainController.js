87baa6793ac319bf7dfdd107afa68deed9031be6
ebabel-eu <hello@ebabel.eu>
2014-08-26 21:35:27 +0200

    UPDATED working on login.

--- src/js/controllers/mainController.js
+++ src/js/controllers/mainController.js
@@ -33,26 +33,17 @@ musicQuizApp.controller('mainController', ['$scope', '$location', 'notifications
             //      2. Logged into Facebook, but not your app ('not_authorized')
             //      3. Not logged into Facebook and can't tell if they are logged into your app or not.
             FB.getLoginStatus(function (response) {
-                var redirectToLogin;
-
                 $scope.login.statusChangeCallback(response);
 
                 if (response.status === 'connected') {
                     // Logged into your app and Facebook.
                     $scope.notifications.add($scope.login.notifications.success);
-                    redirectToLogin = false;
                 } else if (response.status === 'not_authorized') {
                     // The person is logged into Facebook, but not your app.
                     $scope.notifications.add($scope.login.notifications.authorizationRequired);
-                    redirectToLogin = true;
                 } else {
                     // The person is not logged into Facebook, so we're not sure if they are logged into this app or not.
                     $scope.notifications.add($scope.login.notifications.loginRequired);
-                    redirectToLogin = true;
-                }
-
-                if (redirectToLogin) {
-                    $scope.$apply(function() { $location.path("/login"); });
                 }
 
                 // Update the scope to make sure notifications are displayed.
