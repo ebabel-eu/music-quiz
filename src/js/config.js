1241360e1e09efac72cbb229997314bb34221fb3
Nadjib Amar <hello@ebabel.eu>
2014-08-28 00:17:17 +0200

    ADDED new pages.

--- src/js/config.js
+++ src/js/config.js
@@ -7,6 +7,9 @@ musicQuizApp
                 .when('/home', {templateUrl: 'views/home.html'})
                 .when('/login', {templateUrl: 'views/login.html'})
                 .when('/logout', {templateUrl: 'views/logout.html'})
+                .when('/privacy-policy', {templateUrl: 'views/privacy-policy.html'})
+                .when('/about-us', {templateUrl: 'views/about-us.html'})
+                .when('/create-new-quiz', {templateUrl: 'views/create-new-quiz.html'})
                 .when('/404', {templateUrl: 'views/404.html'})
                 .otherwise({redirectTo: '/404'});
         }])
